c432102fa0693ecada5a592e8bca3e4812356c98
Talahatu <87047371+Talahatu@users.noreply.github.com>
2023-09-11 13:56:18 +0700

    dttbl

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,60 @@
 @extends('utama')
 
+@section('title')
+    Pengajuan Perjalanan Dinas
+@endsection
+
 @section('isi')
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur a ipsam at esse dolorum dolor perferendis quisquam?
-        Tempore, voluptatibus assumenda ullam dolores necessitatibus, nisi aliquid atque ducimus asperiores et consectetur!
-    </p>
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+    @if (session('status'))
+        <div class="alert alert-success">{{ session('status') }}</div>
+    @endif
+
+
+    <div class="table-responsive" style="overflow: scroll">
+        <table id="myTable" class="table table-striped table-bordered" style="table-layout: fixed">
+            <thead>
+                <tr>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+
+            </tbody>
+        </table>
+    </div>
+@endsection
+@section('javascript')
+    <script>
+        $(document).ready(function() {
+            $('#myTable').DataTable({
+                paging: false,
+                scrollCollapse: true,
+                scrollY: '445px',
+                columns: [{
+                        data: "idOpti"
+                    },
+                    {
+                        data: "namaOpti"
+                    } {
+                        data: "tglRealisasi"
+                    },
+                    {
+                        data: "deadline"
+                    }
+                ]
+            });
+        });
+    </script>
 @endsection
